c799250df3cc236c65391a69a05e2e5eb2720979
JulienQNN <julienqhn@proton.me>
2024-01-21 15:03:21 +0100

    update main.go

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,2 +1,10 @@
+terraform {
+  required_providers {
+    just-webhook = {
+      source = "JulienQNN/just-webhook"
+      version = "0.1.0"
+    }
+  }
+}
 provider "jwb" {
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+
 	"terraform-provider-jwb/internal/provider"
 )
 
@@ -41,11 +42,12 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// NOTE: This is not a typical Terraform Registry provider address,This specific
+		// NOTE: This is not a typical Terraform Registry provider address,
+		// such as registry.terraform.io/hashicorp/hashicups. This specific
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "hashicorp.com/edu/jwb",
+		Address: "hashicorp.com/edu/hashicups-pf",
 		Debug:   debug,
 	}
 
