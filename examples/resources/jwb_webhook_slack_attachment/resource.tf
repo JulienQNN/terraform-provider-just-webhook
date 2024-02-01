b9b225119696a018aabffe363032955d69e571fb
JulienQNN <julienqhn@proton.me>
2024-02-01 21:29:33 +0100

    fix: test and version

--- examples/resources/jwb_webhook_slack_attachment/resource.tf
+++ examples/resources/jwb_webhook_slack_attachment/resource.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     jwb = {
       source  = "JulienQNN/just-webhook"
-      version = "0.2.2"
+      version = "0.2.3"
     }
   }
 }
--- internal/provider/teams_webhook_resource_test.go
+++ internal/provider/teams_webhook_resource_test.go
@@ -14,7 +14,7 @@ func TestTeamsWebhookResource(t *testing.T) {
 			{
 				Config: `
 resource "jwb_webhook_teams" "test" {
-  webhook_url = "https://webhook.site/f33cf6e1-32fe-4317-b60b-7c249b482bb2"
+  webhook_url = "https://webhook.site/c5648699-9649-4018-b07a-aa4343a52b14"
   theme_color = "1BEF1D"
   sections = [{
     title    = "TITLE"
@@ -70,7 +70,7 @@ resource "jwb_webhook_teams" "test" {
 					resource.TestCheckResourceAttr(
 						"jwb_webhook_teams.test",
 						"webhook_url",
-						"https://webhook.site/f33cf6e1-32fe-4317-b60b-7c249b482bb2",
+						"https://webhook.site/c5648699-9649-4018-b07a-aa4343a52b14",
 					),
 					resource.TestCheckResourceAttr(
 						"jwb_webhook_teams.test",
@@ -222,7 +222,7 @@ resource "jwb_webhook_teams" "test" {
 			{
 				Config: `
 resource "jwb_webhook_teams" "test" {
-  webhook_url = "https://webhook.site/f33cf6e1-32fe-4317-b60b-7c249b482bb2"
+  webhook_url = "https://webhook.site/c5648699-9649-4018-b07a-aa4343a52b14"
   theme_color = "1BEF1D"
   sections = [{
     title    = "TITLE"
@@ -280,7 +280,7 @@ resource "jwb_webhook_teams" "test" {
 					resource.TestCheckResourceAttr(
 						"jwb_webhook_teams.test",
 						"webhook_url",
-						"https://webhook.site/f33cf6e1-32fe-4317-b60b-7c249b482bb2",
+						"https://webhook.site/c5648699-9649-4018-b07a-aa4343a52b14",
 					),
 					resource.TestCheckResourceAttr(
 						"jwb_webhook_teams.test",
